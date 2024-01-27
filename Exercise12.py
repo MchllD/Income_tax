de81416125199c818ef94c430a54018b21c637e9
MchllD <mich11dimafelix@gmail.com>
2024-01-27 21:54:43 +0800

    pseudocode

---
+++ Exercise12.py
@@ -0,0 +1,18 @@
+# Exercise 12: Calculate income tax for the given income by adhering to the rules below
+# Taxable Income	Rate (in %)
+# First $10,000	0
+# Next $10,000	10
+# The remaining	20
+
+
+# pseudocode
+
+# Set initial values
+# Display the given income
+# Check income ranges and calculate tax payable accordingly
+# No tax if income is up to 10000
+# Calculate tax for income between 10001 and 20000
+# Calculate tax for income above 20000
+# # Tax for the first 10000
+# Tax for the remaining income above 20000
+# Display the total tax to pay
